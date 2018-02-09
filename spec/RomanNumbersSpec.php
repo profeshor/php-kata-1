adf703c4217ae7a99acb40fe6b537223a3532252
Pablo Pérez <pabloperez@MacBook-Pro-de-Pablo.local>
2018-02-09 17:39:12 -0500

    Primera Prueba número 1

---
+++ spec/RomanNumbersSpec.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace spec;
+
+use RomanNumbers;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RomanNumbersSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(RomanNumbers::class);
+    }
+
+    function it_convert_number_1()
+    {
+        $this->convert(1)->shouldReturn('I');
+    }
+}
---
+++ src/RomanNumbers.php
@@ -0,0 +1,9 @@
+<?php
+
+class RomanNumbers
+{
+    public function convert($argument1)
+    {
+        Return 'I';
+    }
+}
