a4103918738d3c47ebb755af18048ca4208d7d69
Pablo Pérez <pabloperez@MacBook-Pro-de-Pablo.local>
2018-02-09 18:18:34 -0500

    Prueba número 1 a 3

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -4,42 +4,14 @@ class RomanNumbers
 {
     public function convert($decimal_digit)
     {
-        switch ($decimal_digit) {
-          case 1:
-            return 'I';
-            break;
+      $roman_digit = '';
 
-          case 2:
-            return 'II';
-            break;
+      if ($decimal_digit >=1 || $decimal_digit < 3){
+          for ($i=1; $i<=$decimal_digit; $i++){
+            $roman_digit = $roman_digit.'I';
+          }
+      }
 
-          case 3:
-            return 'III';
-            break;
-
-          case 4:
-            return 'IV';
-            break;
-
-          case 5:
-            return 'V';
-            break;
-
-          case 6:
-            return 'VI';
-            break;
-
-          case 7:
-            return 'VII';
-            break;
-
-          case 8:
-            return 'VIII';
-            break;
-
-          default:
-            # code...
-            break;
-        }
+      return $roman_digit;
     }
 }
