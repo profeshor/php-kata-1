4b9e3cfedb19f655980083518f8293a34bfb1b0c
Pablo Pérez <pabloperez@MacBook-Pro-de-Pablo.local>
2018-02-09 18:26:16 -0500

    Prueba número 4 a 8

--- spec/RomanNumbersSpec.php
+++ spec/RomanNumbersSpec.php
@@ -27,4 +27,24 @@ class RomanNumbersSpec extends ObjectBehavior
     {
         $this->convert(3)->shouldReturn('III');
     }
+
+    function it_convert_number_4()
+    {
+        $this->convert(4)->shouldReturn('IV');
+    }
+
+    function it_convert_number_5()
+    {
+        $this->convert(5)->shouldReturn('V');
+    }
+
+    function it_convert_number_7()
+    {
+        $this->convert(7)->shouldReturn('VII');
+    }
+
+    function it_convert_number_8()
+    {
+        $this->convert(8)->shouldReturn('VIII');
+    }
 }
--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -4,10 +4,20 @@ class RomanNumbers
 {
     public function convert($decimal_digit)
     {
-      $roman_digit = '';
-
-      if ($decimal_digit >=1 || $decimal_digit < 3){
+      if ($decimal_digit >=1 && $decimal_digit <= 3){
+          $roman_digit = '';
           for ($i=1; $i<=$decimal_digit; $i++){
+            $roman_digit = $roman_digit.'I';
+          }
+      }
+
+      if ($decimal_digit == 4){
+        return 'IV';
+      }
+
+      if ($decimal_digit >=5 && $decimal_digit <= 8){
+          $roman_digit = 'V';
+          for ($i=6; $i<=$decimal_digit; $i++){
             $roman_digit = $roman_digit.'I';
           }
       }
